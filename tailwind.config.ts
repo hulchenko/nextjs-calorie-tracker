b0e7b45a887afacbc97e35a8ab1240aade52ac9d
Vadym Hulchenko <vadymhulchenko@gmail.com>
2024-09-01 21:04:19 -0300

    update configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,14 +5,6 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-
-      },
-    },
-  },
-  plugins: [],
+  ]
 };
 export default config;
