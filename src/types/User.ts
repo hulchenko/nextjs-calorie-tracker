95fa6a233e295a01d681e7c5d3a5626a7cc8cd90
Vadym Hulchenko <vadymhulchenko@gmail.com>
2024-09-02 19:58:39 -0300

    remove clerk, add new user type for local and db, remove landing page

--- src/types/User.ts
+++ src/types/User.ts
@@ -1,6 +1,12 @@
-export interface User {
+export interface UserDB {
   user_id: string;
   first_name: string;
-  last_name: number;
+  last_name: string;
+  email: string;
+}
+
+export interface User {
+  firstName: string;
+  lastName: string;
   email: string;
 }
