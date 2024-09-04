6668cbb0e2ee41cdb26dc7c9a15b2878268a3813
Vadym Hulchenko <vadymhulchenko@gmail.com>
2024-09-04 01:12:15 -0300

    add password encryption logic

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,20 @@
+"use server";
+
+import bcrypt from 'bcrypt';
+
 export const passwordValidator = (password: string): boolean => {
     const regex = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}$/gm);
     return regex.test(password);
+}
+
+export const encryptPassword = async (password: string): Promise<string> => {
+    const saltRounds = 10;
+    console.log(`ORIGINAL PASSWORD: `, password);
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    console.log(`HASHED PASSWORD: `, hashedPassword);
+    return hashedPassword;
+}
+
+export const validateHashedPassword = (password: string, hashedPassword: string): boolean => {
+    return bcrypt.compareSync(password, hashedPassword);
 }
