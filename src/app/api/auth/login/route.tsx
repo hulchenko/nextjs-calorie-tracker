90bb22bb5d8d4f6e73646a0ca9fe1a794c777bd0
Vadym Hulchenko <vadymhulchenko@gmail.com>
2024-09-04 17:37:44 -0300

    rework and improve /auth/login api

--- src/app/api/auth/login/route.tsx
+++ src/app/api/auth/login/route.tsx
@@ -2,7 +2,8 @@ import { NextRequest, NextResponse } from 'next/server';
 import { getUser } from '@/server/getUser';
 import { validateHashedPassword } from '@/lib/utils';
 
-export async function POST(req: NextRequest, res: NextResponse) {
+export async function POST(req: NextRequest) {
+
   try {
     const body = await req.json();
     const { email, password } = body;
@@ -10,17 +11,24 @@ export async function POST(req: NextRequest, res: NextResponse) {
     console.log(`PASSWORD: `, password);
 
     const user = await getUser(email, false);
-    const isPasswordCorrect = validateHashedPassword(password, user.password);
-
     console.log(`USER: `, user);
+
+    if (!user) {
+      return NextResponse.json({error: 'User does not exist. Please sign up to proceed.'}, { status: 404 })
+    }
+
+    const isPasswordCorrect = await validateHashedPassword(password, user?.password);
     console.log(`isPasswordCorrect: `, isPasswordCorrect);
 
-    if (user && isPasswordCorrect) {
+
+    if (isPasswordCorrect) {
       return NextResponse.json({ success: true });
     } else {
       return NextResponse.json({ error: 'Invalid credentials.' }, { status: 401 });
     }
+
   } catch (error) {
+    console.error('Error during login:', error);
     return NextResponse.json({ error: 'User login failed.' }, { status: 500 });
   }
 }
--- src/components/SignInForm.tsx
+++ src/components/SignInForm.tsx
@@ -1,42 +1,76 @@
 "use client";
 
-import { FormEvent } from 'react';
+import { FormEvent, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { useToast } from '@chakra-ui/react';
 
 const SignInForm = () => {
+    const toast = useToast();
     const router = useRouter();
 
-    const submitHandler = async(event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isFormValid, setIsFormValid] = useState({
+        email: true,
+        password: true
+    });
 
-        const formData = new FormData(event.currentTarget);
-        const email = formData.get('email');
-        const password = formData.get('password');
-        
+    const submitHandler = async(event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();        
         console.log(`PARAMS: ${email}, ${password}`)
 
-        const response = await fetch('/api/auth/login', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json'},
-            body: JSON.stringify({email, password})
-        })
-        if (response.ok){
-            console.log(`RESPONSE: `, response);
-            router.push('/dashboard')
+        if (!email){
+            setIsFormValid((prevState) => ({...prevState, email: false}));
+        }
+
+        if (!password){
+            setIsFormValid((prevState) => ({...prevState, password: false}));
+        }
+
+        const validParams = email && password;
+
+        if (validParams){
+            try {
+                const response = await fetch('/api/auth/login', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json'},
+                    body: JSON.stringify({email, password})
+                })
+                
+                if (response.ok){
+                    router.push('/dashboard')
+                    toast({title: 'Logged in', status: 'success'})
+                } else {
+                    const { error } = await response.json()
+                    toast({title: `${error}`, status: 'error'})
+                }
+
+            } catch (error) {
+                console.error('Error occured during login:', error)
+            }
         }
     }
 
 
     return (
-        <form onSubmit={submitHandler} className='bg-white shadow-md rounded px-8 py-8 mb-4 max-w-md flex flex-col'>
-            <div className='mb-4'>
-            <input className='shadow border rounded w-full py-3 px-3 text-gray-700  focus:outline-teal-700' type="email" name='email' placeholder='Email' />
-            </div>
-            <div className='mb-6'>
-            <input className='shadow border rounded w-full py-3 px-3 text-gray-700  focus:outline-teal-700' type="password" name='password' placeholder='Password' />
-            </div>
-            <button className='bg-teal-700 hover:bg-teal-600  text-white py-2 px-4 rounded' type='submit'>Sign In</button>
-        </form>
+        <>
+        {/* Signin form using React */}
+            <form onSubmit={submitHandler} className='bg-white shadow-md rounded px-8 py-8 mb-4 max-w-md flex flex-col'>
+                <div className='mb-4'>
+                <input onChange={e => setEmail(e.target.value)} className='shadow border rounded w-full py-3 px-3 text-gray-700  focus:outline-teal-700' type="email" name='email' placeholder='Email' />
+                </div>
+                {!email && !isFormValid.email && (
+                    <span className='text-red-500 text-sm text-center mb-6'>Email cannot be empty</span> 
+                )}
+                <div className='mb-6'>
+                <input onChange={e => setPassword(e.target.value)} className='shadow border rounded w-full py-3 px-3 text-gray-700  focus:outline-teal-700' type="password" name='password' placeholder='Password' />
+                </div>
+                {!password && !isFormValid.password && (
+                    <span className='text-red-500 text-sm text-center mb-6'>Password cannot be empty</span> 
+                )}
+                <button className='bg-teal-700 hover:bg-teal-600  text-white py-2 px-4 rounded' type='submit'>Sign In</button>
+            </form>
+        </>
      );
 }
  
