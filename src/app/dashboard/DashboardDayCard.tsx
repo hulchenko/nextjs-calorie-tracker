5b870cbf208c23d268e21058a7ca6876bbd37f9d
Vadym Hulchenko <vadymhulchenko@gmail.com>
2024-09-29 21:43:13 -0300

    update calendar UI

--- src/app/dashboard/DashboardDayCard.tsx
+++ src/app/dashboard/DashboardDayCard.tsx
@@ -19,14 +19,9 @@ const DashboardDayCard = ({data}) => {
     return ( 
         <li>
             <Link href={{ pathname: `/day/${date}`, query: { user_id } }}>
-                <Card h={32} className='hover:shadow-teal-700 hover:shadow-md w-80 m-4 h-36'>
-                    <CardBody>
-                        <Text className='flex justify-end self-end text-gray-500 text-xl'>{displayDate}</Text>
-                    </CardBody>
-                    <CardHeader className='flex text-teal-600 text-3xl font-bold'>
-                        <Text>{weekDay}</Text>
-                        { isGoalMet && <FontAwesomeIcon className='pl-4' icon={faCheck} /> }
-                    </CardHeader>
+                <Card h={32} className='hover:shadow-teal-700 hover:shadow-md w-80 m-4 h-36'  bg={isGoalMet ? 'teal.600' : 'white'}>
+                    <CardBody className='flex justify-end self-end text-xl' textColor={isGoalMet ? 'white' : 'gray.500'}>{displayDate}</CardBody>
+                    <CardHeader className='flex text-4xl font-bold' textColor={isGoalMet ? 'white' : 'teal.600'} p={0} mb={6} ml={4}>{weekDay}</CardHeader>
                 </Card>
             </Link>
         </li>
