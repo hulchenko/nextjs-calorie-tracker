1282d28357ee29018d10eac3a59043ca223b20ee
Vadym Hulchenko <vadymhulchenko@gmail.com>
2024-10-02 03:50:48 -0300

    add get user api route

--- src/app/api/db/user/route.tsx
+++ src/app/api/db/user/route.tsx
@@ -1,10 +1,21 @@
-import { updateUser } from '@/db/userActions';
+import { getUser, updateUser } from '@/db/userActions';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(request: NextRequest){
     try {
         const { user } = await request.json();
         const response = await updateUser(user);
+        return NextResponse.json(response);        
+    } catch (error) {
+        return NextResponse.json({error: `${error}`}, {status: 500})
+    }
+}
+
+export async function GET(request: NextRequest){
+    const email = request.nextUrl.searchParams.get('email');
+
+    try {
+        const response = await getUser(email as string, false);
         return NextResponse.json(response);        
     } catch (error) {
         return NextResponse.json({error: `${error}`}, {status: 500})
