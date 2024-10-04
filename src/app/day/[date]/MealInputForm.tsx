9a969c8c2cde7faaced25f1dfb5f2d074a91f350
Vadym Hulchenko <vadymhulchenko@gmail.com>
2024-10-04 11:44:29 -0300

    update inputs outline

--- src/app/day/[date]/MealInputForm.tsx
+++ src/app/day/[date]/MealInputForm.tsx
@@ -1,155 +1,213 @@
-import { getFoodData, defaultMeal } from '@/lib/mealUtils';
+import { getFoodData, defaultMeal } from "@/lib/mealUtils";
 import {
-    Button,
-    FormControl,
-    FormLabel,
-    Input,
-    InputGroup,
-    Modal,
-    ModalBody,
-    ModalCloseButton,
-    ModalContent,
-    ModalFooter,
-    ModalHeader,
-    ModalOverlay,
-    Select,
-    Spinner,
-    Stack,
-    Text,
-    useDisclosure,
-    useToast
-} from '@chakra-ui/react';
-import { faPlus, faUtensils } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useContext, useEffect, useState } from 'react';
-import { MealContext } from './DayForm';
-import React from 'react';
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Select,
+  Spinner,
+  Stack,
+  Text,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react";
+import { faPlus, faUtensils } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useContext, useEffect, useState } from "react";
+import { MealContext } from "./DayForm";
+import React from "react";
 
 const MealInputForm = () => {
-    const toast = useToast();
-    const {mealList, setMealList, setSaveReady} = useContext(MealContext);
+  const toast = useToast();
+  const { mealList, setMealList, setSaveReady } = useContext(MealContext);
 
-    const [query, setQuery] = useState('');
-    const [searched, setSearched] = useState(false);
-    const [delayedFetch, setDelayedFetch] = useState(query)
-    const [meal, setMeal] = useState(defaultMeal());
-    const [loading, setLoading] = useState(false);
+  const [query, setQuery] = useState("");
+  const [searched, setSearched] = useState(false);
+  const [delayedFetch, setDelayedFetch] = useState(query);
+  const [meal, setMeal] = useState(defaultMeal());
+  const [loading, setLoading] = useState(false);
 
-    const { isOpen, onOpen, onClose } = useDisclosure(); // modal props
+  const { isOpen, onOpen, onClose } = useDisclosure(); // modal props
 
-    useEffect(() => {
-        // search and get nutritions
-        if(query.length >= 3 && delayedFetch){
-            const getFood = async () => {
-                await getFoodData(query, setMeal, setLoading, toast);
-                setSearched(true);
-            };
-            getFood();
-        }
-    }, [delayedFetch])
-
-    useEffect(() => {
-        setSearched(false);
-        const delayHandler = setTimeout(() => setDelayedFetch(query), 1500); // delay fetch by 1.5 seconds after the user interracted with the input field
-        return () => clearTimeout(delayHandler); // reset if the user starts typing again
-    }, [query]);
-
-    useEffect(() => {
-        if(!isOpen){
-            setMeal(defaultMeal()) // clean up modal if it wasn't submitted
-        }
-    }, [isOpen])
-
-    const mealAddHandler = (event) => {
-        event?.preventDefault();
-
-        setMealList(prev => [...prev, meal])
-        setSaveReady(true);
-        setSearched(false);
+  useEffect(() => {
+    // search and get nutritions
+    if (query.length >= 3 && delayedFetch) {
+      const getFood = async () => {
+        await getFoodData(query, setMeal, setLoading, toast);
+        setSearched(true);
+      };
+      getFood();
     }
-    
-    return (
-        <>
-        {mealList?.length === 0 && (
-                <div  className='flex flex-col items-center'>
-                    <h1 className='text-4xl'>No meals</h1>
-                    <p className='text-base text-gray-500 py-4'>Looks like you haven't added any meals yet.</p>
-                    <button className='bg-teal-700 hover:bg-teal-600  text-white py-4 p-6 mt-2 rounded' onClick={onOpen}>Add Meal<FontAwesomeIcon className='ml-2' icon={faUtensils} /></button> 
-                </div>
-        )}
-        {mealList?.length > 0 && mealList?.length < 9 && (
-                <button className='fixed bottom-40 right-52 bg-teal-700 hover:bg-teal-600 text-white py-4 p-6 mt-2 rounded' onClick={onOpen}><FontAwesomeIcon icon={faPlus} /></button>
-        )}
-            <Modal isOpen={isOpen} onClose={onClose} scrollBehavior='outside' isCentered>
-                <ModalOverlay />
-                    <ModalContent>
-                        <form onSubmit={mealAddHandler}>
-                            <ModalHeader className='text-teal-700'>Add a New Meal</ModalHeader>
-                            <ModalCloseButton size='lg'/>
-                            <ModalBody pb={6}>
-                                <FormControl>
-                                    <FormLabel>Type</FormLabel>
-                                    <InputGroup size='lg'>
-                                        <Select placeholder='Please Select' onChange={(e) => setMeal({...meal, meal_type: e.target.value})}>
-                                            <option value='Breakfast'>Breakfast</option>
-                                            <option value='Brunch'>Brunch</option>
-                                            <option value='Lunch'>Lunch</option>
-                                            <option value='Supper'>Supper</option>
-                                            <option value='Dinner'>Dinner</option>
-                                            <option value='Midnight Snack'>Midnight Snack</option>
-                                            <option value='Other'>Other</option>
-                                        </Select>
-                                    </InputGroup>
-                                </FormControl>
+  }, [delayedFetch]);
 
-                                <FormControl mt={4}>
-                                    <FormLabel>Description</FormLabel>
-                                    <InputGroup size='lg'>
-                                        <Input placeholder='e.g. 2 eggs and a toast' onChange={(e) => {setQuery(e.target.value), setMeal({...meal, meal_description: e.target.value})}}/>
-                                    </InputGroup>
-                                </FormControl>
+  useEffect(() => {
+    setSearched(false);
+    const delayHandler = setTimeout(() => setDelayedFetch(query), 1500); // delay fetch by 1.5 seconds after the user interracted with the input field
+    return () => clearTimeout(delayHandler); // reset if the user starts typing again
+  }, [query]);
 
-                                <FormControl mt={4}>
-                                    <FormLabel>Nutrition List</FormLabel>
-                                    <Stack className={`border rounded p-4 ${meal.items.length ? 'border-teal-600' : 'border-gray-200'}`}>
-                                        {loading && <span className='w-full flex text-center justify-center text-teal-600'><Spinner/></span>}
-                                        {meal.items.length && meal.items.map(i => (
-                                                    <>
-                                                        <Text>Ingridient(s): <b>{i?.name}</b></Text>
-                                                        <Text>Calories: {i?.calories}</Text>
-                                                        <Text>Carbohydrates: {i?.carbohydrates_total_g} g</Text>
-                                                        <Text>Cholesterol: {i?.cholesterol_mg} mg</Text>
-                                                        <Text>Saturated Fat: {i?.fat_saturated_g} g</Text>
-                                                        <Text>Total Fat: {i?.fat_total_g} g</Text>
-                                                        <Text>Fiber: {i?.fiber_g} g</Text>
-                                                        <Text>Potassium: {i?.potassium_mg} mg</Text>
-                                                        <Text>Protein: {i?.protein_g} g</Text>
-                                                        <Text>Serving Size: {i?.serving_size_g} g</Text>
-                                                        <Text>Sodium: {i?.sodium_mg} mg</Text>
-                                                        <Text>Sugar: {i?.sugar_g} g</Text>
-                                                    </>
-                                                ))}
-                                        {!meal.items.length && searched && <Text className='text-orange-500'>Nothing is found. Update description and try again.</Text>}
-                                    </Stack>
-                                </FormControl>
+  useEffect(() => {
+    if (!isOpen) {
+      setMeal(defaultMeal()); // clean up modal if it wasn't submitted
+    }
+  }, [isOpen]);
 
-                                <FormControl mt={4}>
-                                    <FormLabel>Total Calories: <b className='text-teal-600'>{meal?.calories || 0}</b></FormLabel>
-                                </FormControl>
-                            </ModalBody>
-                            
+  const mealAddHandler = (event) => {
+    event?.preventDefault();
 
-                            <ModalFooter>
-                                <Button colorScheme='teal' mr={3} type='submit' isDisabled={meal.items.length === 0 || meal.meal_type === ''} onClick={onClose}>
-                                    Add
-                                </Button>
-                                <Button onClick={onClose}>Cancel</Button>
-                            </ModalFooter>
-                        </form>
-                    </ModalContent>
-            </Modal>
-        </>
-    )
-}
+    setMealList((prev) => [...prev, meal]);
+    setSaveReady(true);
+    setSearched(false);
+  };
+
+  return (
+    <>
+      {mealList?.length === 0 && (
+        <div className="flex flex-col items-center">
+          <h1 className="text-4xl">No meals</h1>
+          <p className="text-base text-gray-500 py-4">
+            Looks like you haven't added any meals yet.
+          </p>
+          <button
+            className="bg-teal-700 hover:bg-teal-600  text-white py-4 p-6 mt-2 rounded"
+            onClick={onOpen}
+          >
+            Add Meal
+            <FontAwesomeIcon className="ml-2" icon={faUtensils} />
+          </button>
+        </div>
+      )}
+      {mealList?.length > 0 && mealList?.length < 9 && (
+        <button
+          className="fixed bottom-40 right-52 bg-teal-700 hover:bg-teal-600 text-white py-4 p-6 mt-2 rounded"
+          onClick={onOpen}
+        >
+          <FontAwesomeIcon icon={faPlus} />
+        </button>
+      )}
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        scrollBehavior="outside"
+        isCentered
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <form onSubmit={mealAddHandler}>
+            <ModalHeader className="text-teal-700">Add a New Meal</ModalHeader>
+            <ModalCloseButton size="lg" />
+            <ModalBody pb={6}>
+              <FormControl>
+                <FormLabel>Type</FormLabel>
+                <InputGroup size="lg">
+                  <Select
+                    focusBorderColor="teal.600"
+                    placeholder="Please Select"
+                    onChange={(e) =>
+                      setMeal({ ...meal, meal_type: e.target.value })
+                    }
+                  >
+                    <option value="Breakfast">Breakfast</option>
+                    <option value="Brunch">Brunch</option>
+                    <option value="Lunch">Lunch</option>
+                    <option value="Supper">Supper</option>
+                    <option value="Dinner">Dinner</option>
+                    <option value="Midnight Snack">Midnight Snack</option>
+                    <option value="Other">Other</option>
+                    Option
+                  </Select>
+                </InputGroup>
+              </FormControl>
+
+              <FormControl mt={4}>
+                <FormLabel>Description</FormLabel>
+                <InputGroup size="lg">
+                  <Input
+                    focusBorderColor="teal.600"
+                    placeholder="e.g. 2 eggs and a toast"
+                    onChange={(e) => {
+                      setQuery(e.target.value),
+                        setMeal({
+                          ...meal,
+                          meal_description: e.target.value,
+                        });
+                    }}
+                  />
+                </InputGroup>
+              </FormControl>
+
+              <FormControl mt={4}>
+                <FormLabel>Nutrition List</FormLabel>
+                <Stack
+                  className={`border rounded p-4 ${
+                    meal.items.length ? "border-teal-600" : "border-gray-200"
+                  }`}
+                >
+                  {loading && (
+                    <span className="w-full flex text-center justify-center text-teal-600">
+                      <Spinner />
+                    </span>
+                  )}
+                  {meal.items.length &&
+                    meal.items.map((i) => (
+                      <>
+                        <Text>
+                          Ingridient(s): <b>{i?.name}</b>
+                        </Text>
+                        <Text>Calories: {i?.calories}</Text>
+                        <Text>Carbohydrates: {i?.carbohydrates_total_g} g</Text>
+                        <Text>Cholesterol: {i?.cholesterol_mg} mg</Text>
+                        <Text>Saturated Fat: {i?.fat_saturated_g} g</Text>
+                        <Text>Total Fat: {i?.fat_total_g} g</Text>
+                        <Text>Fiber: {i?.fiber_g} g</Text>
+                        <Text>Potassium: {i?.potassium_mg} mg</Text>
+                        <Text>Protein: {i?.protein_g} g</Text>
+                        <Text>Serving Size: {i?.serving_size_g} g</Text>
+                        <Text>Sodium: {i?.sodium_mg} mg</Text>
+                        <Text>Sugar: {i?.sugar_g} g</Text>
+                      </>
+                    ))}
+                  {!meal.items.length && searched && (
+                    <Text className="text-orange-500">
+                      Nothing is found. Update description and try again.
+                    </Text>
+                  )}
+                </Stack>
+              </FormControl>
+
+              <FormControl mt={4}>
+                <FormLabel>
+                  Total Calories:{" "}
+                  <b className="text-teal-600">{meal?.calories || 0}</b>
+                </FormLabel>
+              </FormControl>
+            </ModalBody>
+
+            <ModalFooter>
+              <Button
+                colorScheme="teal"
+                mr={3}
+                type="submit"
+                isDisabled={meal.items.length === 0 || meal.meal_type === ""}
+                onClick={onClose}
+              >
+                Add
+              </Button>
+              <Button onClick={onClose}>Cancel</Button>
+            </ModalFooter>
+          </form>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
 
 export default MealInputForm;
--- src/app/login/SignInForm.tsx
+++ src/app/login/SignInForm.tsx
@@ -1,77 +1,100 @@
-'use client';
+"use client";
 
-import { useToast } from '@chakra-ui/react';
-import { useRouter } from 'next/navigation';
-import { FormEvent, useState } from 'react';
-import { useSession } from '../context/SessionProvider';
-import React from 'react';
+import { useToast } from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
+import { useSession } from "../context/SessionProvider";
+import React from "react";
 
 const SignInForm = () => {
-    const toast = useToast();
-    const router = useRouter();
+  const toast = useToast();
+  const router = useRouter();
 
-    const { setSession } = useSession();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [isFormValid, setIsFormValid] = useState({
-        email: true,
-        password: true
-    });
+  const { setSession } = useSession();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isFormValid, setIsFormValid] = useState({
+    email: true,
+    password: true,
+  });
 
-    const submitHandler = async(event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
+  const submitHandler = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-        if (!email){
-            setIsFormValid((prevState) => ({...prevState, email: false}));
-        }
-
-        if (!password){
-            setIsFormValid((prevState) => ({...prevState, password: false}));
-        }
-
-        const validParams = email && password;
-
-        if (validParams){
-            try {
-                const response = await fetch('/api/auth/login', {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json'},
-                    body: JSON.stringify({email, password})
-                })
-                if (!response.ok){
-                    const {error} = await response.json();
-                    throw error;
-                }
-                const sessionData = await response.json();
-                setSession(sessionData);
-                router.push('/dashboard'); 
-                toast({title: 'Signed in', status: 'info'});
-            } catch (error) {
-                toast({title: `${error}`, status: 'error'});
-            }
-        }
+    if (!email) {
+      setIsFormValid((prevState) => ({ ...prevState, email: false }));
     }
 
+    if (!password) {
+      setIsFormValid((prevState) => ({ ...prevState, password: false }));
+    }
 
-    return (
-        <>
-            <form onSubmit={submitHandler} className='bg-white shadow-md rounded px-8 py-8 mb-4 max-w-md flex flex-col'>
-                <div className='mb-6'>
-                    <input onChange={e => setEmail(e.target.value)} className='shadow border rounded w-full py-3 px-3 text-gray-600  focus:outline-teal-700' type="email" name='email' placeholder='Email' />
-                    {!email && !isFormValid.email && (
-                        <span className='text-red-500 mt-1 text-sm block'>Email cannot be empty</span> 
-                    )}
-                </div>
-                <div className='mb-6'>
-                    <input onChange={e => setPassword(e.target.value)} className='shadow border rounded w-full py-3 px-3 text-gray-600  focus:outline-teal-700' type="password" name='password' placeholder='Password' />
-                    {!password && !isFormValid.password && (
-                        <span className='text-red-500 mt-1 text-sm block'>Password cannot be empty</span> 
-                    )}
-                </div>
-                <button className='bg-teal-700 hover:bg-teal-600  text-white py-2 px-4 rounded' type='submit'>Sign In</button>
-            </form>
-        </>
-     );
-}
- 
+    const validParams = email && password;
+
+    if (validParams) {
+      try {
+        const response = await fetch("/api/auth/login", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ email, password }),
+        });
+        if (!response.ok) {
+          const { error } = await response.json();
+          throw error;
+        }
+        const sessionData = await response.json();
+        setSession(sessionData);
+        router.push("/dashboard");
+        toast({ title: "Signed in", status: "info" });
+      } catch (error) {
+        toast({ title: `${error}`, status: "error" });
+      }
+    }
+  };
+
+  return (
+    <>
+      <form
+        onSubmit={submitHandler}
+        className="bg-white shadow-md rounded px-8 py-8 mb-4 max-w-md flex flex-col"
+      >
+        <div className="mb-6">
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            className="shadow border rounded w-full py-3 px-3 text-gray-600  focus:outline-teal-600"
+            type="email"
+            name="email"
+            placeholder="Email"
+          />
+          {!email && !isFormValid.email && (
+            <span className="text-red-500 mt-1 text-sm block">
+              Email cannot be empty
+            </span>
+          )}
+        </div>
+        <div className="mb-6">
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            className="shadow border rounded w-full py-3 px-3 text-gray-600  focus:outline-teal-600"
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
+          {!password && !isFormValid.password && (
+            <span className="text-red-500 mt-1 text-sm block">
+              Password cannot be empty
+            </span>
+          )}
+        </div>
+        <button
+          className="bg-teal-700 hover:bg-teal-600  text-white py-2 px-4 rounded"
+          type="submit"
+        >
+          Sign In
+        </button>
+      </form>
+    </>
+  );
+};
+
 export default SignInForm;
