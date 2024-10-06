e3e2e1827370a0fb6b33f40806c3aacb6f1fc43f
Vadym Hulchenko <vadymhulchenko@gmail.com>
2024-10-05 21:57:10 -0300

    update dashboard day cards for small screens

--- src/app/dashboard/DayCard.tsx
+++ src/app/dashboard/DayCard.tsx
@@ -24,24 +24,38 @@ const DayCard = ({ data }) => {
         className="hover:shadow-teal-700 hover:shadow-md m-2 sm:m-3 w-auto md:w-60 h-96"
         bg={isGoalMet ? "teal.600" : "white"}
       >
-        <CardBody
-          className="flex justify-end self-end sm:text-xl"
-          textColor={isGoalMet ? "white" : "gray.500"}
-          p={window.innerWidth <= 640 ? 0 : 5}
-          mr={window.innerWidth <= 640 ? 4 : 0}
-          mt={window.innerWidth <= 640 ? 2 : 0}
-        >
-          {isToday ? "Today" : ""} {displayDate}
-        </CardBody>
-        <CardHeader
-          className="flex sm:text-4xl font-bold"
-          textColor={isGoalMet ? "white" : "teal.600"}
-          p={0}
-          mb={6}
-          ml={4}
-        >
-          {weekDay}
-        </CardHeader>
+        <div className="hidden sm:block">
+          <CardBody
+            className="flex items-center self-end sm:text-xl"
+            textColor={isGoalMet ? "white" : "gray.500"}
+            p={window.innerWidth <= 640 ? 0 : 5}
+            mr={window.innerWidth <= 640 ? 4 : 0}
+            mt={window.innerWidth <= 640 ? 2 : 0}
+          >
+            {isToday ? "Today" : ""} {displayDate}
+          </CardBody>
+          <CardHeader
+            className="flex sm:text-4xl font-bold"
+            textColor={isGoalMet ? "white" : "teal.600"}
+            p={0}
+            mb={6}
+            ml={4}
+          >
+            {weekDay}
+          </CardHeader>
+        </div>
+        <div className="sm:hidden flex justify-between px-8 text-2xl h-full items-center">
+          <p
+            className={`${
+              isGoalMet ? "text-white" : "text-teal-600"
+            } font-bold`}
+          >
+            {weekDay}
+          </p>
+          <p className={`${isGoalMet ? "text-white" : "text-gray-500"}`}>
+            {isToday ? "Today" : ""} {displayDate}
+          </p>
+        </div>
       </Card>
     </Link>
   );
